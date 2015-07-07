26990920ee3df37237b932bfcd9415477f7a5604
eduponte <ponteeduard@gmail.com>
2015-07-07 16:05:52 +0200

    Add validations

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,13 +15,14 @@ exports.load = function(req, res, next, quizId){
 };
 
 exports.show = function(req, res) {
-  res.render('quizes/show', {quiz: req.quiz});
+  res.render('quizes/show', {quiz: req.quiz, errors: []});
 }
 
 exports.answer = function(req, res) {
   res.render('quizes/answer', {
     quiz: req.quiz,
-    respuesta: ((req.query.respuesta === req.quiz.respuesta) ? 'Correcto' : 'Incorrecto')
+    respuesta: ((req.query.respuesta.toUpperCase() === req.quiz.respuesta.toUpperCase()) ? 'Correcto' : 'Incorrecto'),
+		errors: []
   });
 }
 
@@ -30,8 +31,8 @@ exports.index = function(req, res) {
 		where: ['pregunta like ?', '%'+req.query.search.replace(' ','%')+'%'],
 		order: [['pregunta', 'ASC']]
 	} : {};
-  models.Quiz.findAll(findOptions).success(function(quizes) {
-    res.render('quizes/index.ejs', {quizes: quizes});
+  models.Quiz.findAll(findOptions).then(function(quizes) {
+    res.render('quizes/index.ejs', {quizes: quizes, errors: []});
   });
 }
 
@@ -40,16 +41,22 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
-	quiz.save({fields: ['pregunta','respuesta']}).then(function() {
-		res.redirect('/quizes');
+	quiz.validate().then(function(err) {
+		if (err) {
+			res.render('quizes/new', {quiz: quiz, errors:err.errors});
+		} else {
+			quiz.save({fields: ['pregunta','respuesta']}).then(function() {
+				res.redirect('/quizes');
+			});
+		}
 	});
 };
 
 exports.author = function(req, res) {
-  res.render('author');
+  res.render('author', {errors: []});
 }
--- models/models.js
+++ models/models.js
@@ -29,16 +29,16 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 // initialize quiz table
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
   // runs handler once created
-  Quiz.count().success(function (count){
+  Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create(
         {pregunta: 'Capital de Italia',   respuesta: 'Roma'}
       );
       Quiz.create(
         {pregunta: 'Capital de Portugal',   respuesta: 'Lisboa'}
-      ).success(function(){console.log('Base de datos inicializada')});
+      ).then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
